5dae60a75ab7b9aea5a84b3464540931bd559532
chani <cucute8878@naver.com>
2024-08-20 14:35:06 +0900

    Initial setting

---
+++ src/main/java/com/nggg/ng3/Ng3Application.java
@@ -0,0 +1,13 @@
+package com.nggg.ng3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ng3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ng3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nggg/ng3/Ng3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nggg.ng3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ng3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
