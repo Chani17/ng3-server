497ec0a5e04ebf035c2c2e51cc6133782085407b
Chani17 <cucute8878@naver.com>
2024-08-27 16:39:27 +0900

    add: AvatarComponent entity

---
+++ src/main/java/com/nggg/ng3/entity/AvatarComponent.java
@@ -0,0 +1,37 @@
+package com.nggg.ng3.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "AVATAR_COMPONENTS")
+public class AvatarComponent {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String imageUrl;
+
+	@ManyToOne
+	@JoinColumn(name = "AVATAR_COMPONENT_CATEGORY_ID", nullable = false)
+	private AvatarComponentCategory avatarComponentCategory;
+}
