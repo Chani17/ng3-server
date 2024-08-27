05c005b5808d2c75e1861e162289af8018ded218
chani <66076741+Chani17@users.noreply.github.com>
2024-08-27 16:55:26 +0900

    Hotfix/#9/add avatars entity (#12)
    * Feature/#1/websocket (#6)
    
    * Style: dependency
    
    * Add: websocket dependency
    
    * Feat: share canvas with websocket
    
    * Update: ddl-auto setting value
    
    * Update: mysql database dependency
    
    * add: constructor annotation in word entity
    
    * add: Avatar entity
    
    * add: AvatarComponentCategory entity
    
    * add: AvatarComponent entity
    
    * add: wearing entity

--- src/main/java/com/nggg/ng3/entity/AvatarComponent.java
+++ src/main/java/com/nggg/ng3/entity/AvatarComponent.java
@@ -1,6 +1,5 @@
 package com.nggg.ng3.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,21 +14,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder(toBuilder = true)
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 @Entity
-@Table(name = "WORDS")
-public class Word {
+@Table(name = "AVATAR_COMPONENTS")
+public class AvatarComponent {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String content;
+	@Column(nullable = false)
+	private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "THEME_ID", nullable = false)
-    private Theme theme;
+	@Column(nullable = false)
+	private String imageUrl;
+
+	@ManyToOne
+	@JoinColumn(name = "AVATAR_COMPONENT_CATEGORY_ID", nullable = false)
+	private AvatarComponentCategory avatarComponentCategory;
 }
