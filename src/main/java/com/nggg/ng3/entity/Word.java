b60b8790e0dec2187e87eadc4868af25165e337c
Chani17 <cucute8878@naver.com>
2024-08-27 16:30:47 +0900

    add: constructor annotation in word entity

--- src/main/java/com/nggg/ng3/entity/Word.java
+++ src/main/java/com/nggg/ng3/entity/Word.java
@@ -9,11 +9,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "WORDS")
 public class Word {
