7b14896812fb8d20169921d38e06fa8b363f07c5
HyeokJinPark <phj970701@gmail.com>
2024-08-28 15:18:49 +0900

    FEAT: DB에 없는 Oauth2 로그인 유저 DB에 저장
    name, nickname은 수정 필요

---
+++ src/main/java/com/nggg/ng3/model/UserDTO.java
@@ -0,0 +1,10 @@
+package com.nggg.ng3.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserDTO {
+    private String loginUserId;
+}
---
+++ src/main/java/com/nggg/ng3/service/UserService.java
@@ -0,0 +1,30 @@
+package com.nggg.ng3.service;
+
+import com.nggg.ng3.entity.User;
+import com.nggg.ng3.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    /*해당 ID의 유저가 db에 없다면 새로 넣기*/
+    public void addUser(String loginUserId){
+        Optional<User> user = userRepository.findById(loginUserId);
+        if(user.isEmpty()){
+            User newUser = User.builder()
+                    .email(loginUserId)
+                    .name("이름")
+                    .nickname("닉네임")
+                    .build();
+            userRepository.save(newUser);
+        }
+    }
+}
