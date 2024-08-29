c352daf4d8b2d2c20182e8e3e997780b392adb14
hssoomin <hssoomin@gmail.com>
2024-08-30 03:20:54 +0900

    Update FollowController.java
    와일드카드 해제

--- src/main/java/com/nggg/ng3/controller/FollowController.java
+++ src/main/java/com/nggg/ng3/controller/FollowController.java
@@ -7,7 +7,12 @@ import com.nggg.ng3.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
